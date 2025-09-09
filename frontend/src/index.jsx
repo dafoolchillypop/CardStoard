82ce788e72ed58f9e75defaccbb19f7993a0ad23
shawn <shawn@HISGAME1.localdomain>
2025-09-08 22:33:19 -0400

    updating css

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css';
-import HomePage from './pages/HomePage';
-import AddCard from './pages/AddCard';
-import ListCards from './pages/ListCards';
-import UpdateCard from './pages/UpdateCard';
-import DeleteCard from './pages/DeleteCard';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import AddCard from "./pages/AddCard";
+import ListCards from "./pages/ListCards";
+import UpdateCard from "./pages/UpdateCard";
+import DeleteCard from "./pages/DeleteCard";
+import "./index.css";
 
-ReactDOM.render(
+// Import the background image
+import bgImage from "./assets/baseball-bg.png";
+
+// Apply background dynamically
+document.body.style.background = `url(${bgImage}) no-repeat center center fixed`;
+document.body.style.backgroundSize = "cover";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Router>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Home />} />
       <Route path="/add" element={<AddCard />} />
       <Route path="/list" element={<ListCards />} />
       <Route path="/update" element={<UpdateCard />} />
       <Route path="/delete" element={<DeleteCard />} />
     </Routes>
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
